a046c05ec6f66b158b3e2f379ff271ca5b41079d
Javis Sullivan <javis.sullivan@verizondigitalmedia.com>
2019-01-15 13:51:17 -0500

    go fmt from gofmt vim plugin

--- main.go
+++ main.go
@@ -1,81 +1,85 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "os"
-    bitstream "github.com/dgryski/go-bitstream"
+	"fmt"
+	bitstream "github.com/dgryski/go-bitstream"
+	"io"
+	"os"
 )
 
 func check(e error) bool {
-    if e == io.EOF { return true }
-    if e != nil {
-        panic(e)
-    }
-    return false
+	if e == io.EOF {
+		return true
+	}
+	if e != nil {
+		panic(e)
+	}
+	return false
 }
 
 func readData(r io.Reader, cb func(uint32)) {
-    // Read 3 bytes from |r|
-    // If less than 3 bytes are read, pad the remaining bytes with 0b00000000
-    var err error
-    read24 := make([]uint8, 3)
-    for {
-        var bytesread int
-        bytesread, err = r.Read(read24)
-        if check(err) { break }
-        data := uint32(0)
-        switch bytesread {
-           case 3:
-               data = uint32(read24[2]) | uint32(read24[1]) << 8 | uint32(read24[0]) << 16
-           case 2:
-               data = uint32(read24[1]) | uint32(read24[0]) << 8
-           case 1:
-               data = uint32(read24[0])
-           default:
-               panic("I dont think this should happen")
-        }
-        // Send |data| to our callback function; Success!
-        cb(data)
-    }
+	// Read 3 bytes from |r|
+	// If less than 3 bytes are read, pad the remaining bytes with 0b00000000
+	var err error
+	read24 := make([]uint8, 3)
+	for {
+		var bytesread int
+		bytesread, err = r.Read(read24)
+		if check(err) {
+			break
+		}
+		data := uint32(0)
+		switch bytesread {
+		case 3:
+			data = uint32(read24[2]) | uint32(read24[1])<<8 | uint32(read24[0])<<16
+		case 2:
+			data = uint32(read24[1]) | uint32(read24[0])<<8
+		case 1:
+			data = uint32(read24[0])
+		default:
+			panic("I dont think this should happen")
+		}
+		// Send |data| to our callback function; Success!
+		cb(data)
+	}
 }
 
 func main() {
-    fmt.Println("Hello there")
-    r, err := os.Open("/tmp/1")
-    defer r.Close()
-    check(err)
-    pSymbols := map[uint16]uint64{}
-    cSymbols := map[uint16]uint64{}
-    iSymbols := map[uint16]uint64{}
-    readData(r, func(data uint32) {
-        p, c, i := Data2PCI(data)
-        pSymbols[uint16(p)] += 1
-        if p != 21 {
-            cSymbols[c] += 1
-        }
-        if p != 0 {
-            iSymbols[i] += 1
-        }
-    })
-    generateCodesFromCount(pSymbols, cSymbols, iSymbols)
-    fmt.Println(len(pSymbols), len(cSymbols), len(iSymbols))
+	fmt.Println("Hello there")
+	r, err := os.Open("/tmp/1")
+	defer r.Close()
+	check(err)
+	pSymbols := map[uint16]uint64{}
+	cSymbols := map[uint16]uint64{}
+	iSymbols := map[uint16]uint64{}
+	readData(r, func(data uint32) {
+		p, c, i := Data2PCI(data)
+		pSymbols[uint16(p)] += 1
+		if p != 21 {
+			cSymbols[c] += 1
+		}
+		if p != 0 {
+			iSymbols[i] += 1
+		}
+	})
+	generateCodesFromCount(pSymbols, cSymbols, iSymbols)
+	fmt.Println(len(pSymbols), len(cSymbols), len(iSymbols))
 
-    r.Seek(0, 0)
-    w, err := os.OpenFile("/tmp/2", os.O_RDWR|os.O_CREATE, 0755)
-    defer w.Close()
-    check(err)
-    bw := bitstream.NewWriter(w)
+	r.Seek(0, 0)
+	w, err := os.OpenFile("/tmp/2", os.O_RDWR|os.O_CREATE, 0755)
+	defer w.Close()
+	check(err)
+	bw := bitstream.NewWriter(w)
 
-    readData(r, func(data uint32) {
-        p, c, i := Data2PCI(data)
-        Encode(pCodes, pBits, bw, uint16(p))
-        if p != 21 && len(cCodes[p]) > 1 {
-            Encode(cCodes[p], cBits[p], bw, c)
-        }
-        if p != 0 && len(iCodes[p]) > 1 {
-            Encode(iCodes[p], iBits[p], bw, i)
-        }
-    })
-    bw.Flush(bitstream.Bit(false))
+	readData(r, func(data uint32) {
+		p, c, i := Data2PCI(data)
+		Encode(pCodes, pBits, bw, uint16(p))
+		if p != 21 && len(cCodes[p]) > 1 {
+			Encode(cCodes[p], cBits[p], bw, c)
+		}
+		if p != 0 && len(iCodes[p]) > 1 {
+			Encode(iCodes[p], iBits[p], bw, i)
+		}
+	})
+	bw.Flush(bitstream.Bit(false))
 }
